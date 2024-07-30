0eb48317448bf811b36f8a76086ff045888e3386
Gabriel <gabriel.sdiniz@outlook.com>
2024-07-30 17:43:05 -0300

    remove: test controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { LucianoController } from './luciano/luciano.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, LucianoController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
